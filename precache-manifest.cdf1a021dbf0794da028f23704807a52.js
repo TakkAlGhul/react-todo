b7ae64611d9f4634187ae17b24eac5eb4c779004
TakkAlGhul <takuro.mike@gmail.com>
2019-06-09 17:41:51 +0900

    Updates

---
+++ precache-manifest.cdf1a021dbf0794da028f23704807a52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838b3e9053952c1e34ad2b989aea3513",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "8efa3c58ba5137a64d26",
+    "url": "/react-todo/static/css/main.f74d5312.chunk.css"
+  },
+  {
+    "revision": "c2a91e8d5b145ae0b2bd",
+    "url": "/react-todo/static/js/2.f1644570.chunk.js"
+  },
+  {
+    "revision": "8efa3c58ba5137a64d26",
+    "url": "/react-todo/static/js/main.a11df1d4.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  }
+]);
